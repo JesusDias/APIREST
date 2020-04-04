99057f7861ed1ca9a7bbd596b074013c20d80206
JesusDias <jesussouzadias18@gmail.com>
2020-04-04 01:19:37 -0300

    Metodo PUT

--- source/Controllers/User.php
+++ source/Controllers/User.php
@@ -64,6 +64,78 @@ switch($_SERVER["REQUEST_METHOD"]){
         header("HTTP/1.1 201 Created");
         echo \json_encode(array("response" => "Usuário criado com sucesso"));
     break;
+    case "GET":
+        header("HTTP/1.1 200 OK");
+        $users = new User();
+        if($users->find()->Count()>0){
+            $return = array();
+            foreach($users->find()->fetch(true) as $user){
+                //tratamento dos dados vindos do banco
+                array_push($return, $user->data());
+            }
+            echo json_encode(array("response" => $return));
+        } else {
+            echo json_encode(array("response" => "Nenhum usuário localizado"));
+        }
+    break;
+    case "PUT":
+        //pega pra mim o id que foi passado nessa requisiçao
+        $userId = filter_input(INPUT_GET,"id");
+        //Se o id não tiver sido passado mande essa resposta pro usuáro
+        if(!$userId){
+            header("HTTP/1.1 400 Bad Request");
+            echo json_encode(array("response" => "Nenhum id foi informado."));
+            exit;
+        }
+        //Le o que veio na url e converte pra um objeto
+        $data = json_decode(file_get_contents('php://input'), false);
+        if(!$data){
+            header("HTTP/1.1 400 Bad Request");
+            echo json_encode(array("response" => "Nenhum dado informado."));
+            exit;
+        }
+
+        //Se os dados foram passados então valida eles e se algum não passar
+        //jogue no array de erros
+        $erros = array();
+        if(!Validations::validationString($data->first_name)){
+            array_push($erros, "Nome Inválido");
+        }
+        if(!Validations::validationString($data->last_name)){
+            array_push($erros, "Sobrenome Inválido");
+        }
+        if(!Validations::validationEmail($data->email)){
+            array_push($erros, "Email Inválido");
+        }
+
+       //Se houver erros mostre para o usuário quais são os erros e pare a aplicação
+       if(count($erros) > 0){
+        header("HTTP/1.1 400 Bad Request");
+        echo json_encode(array("response" => "Há campos inválidos", "fields" => $erros));
+        exit;
+        }
+
+        //===========================DATALAYER======================================
+        //instancia e ja me retorna o usuário onde o id for igual ao informado na url
+        //na variável $userId
+        $user = (new User())->findById($userId);
+        $user->first_name = $data->first_name;
+        $user->last_name = $data->last_name;
+        $user->email = $data->email;
+        $user->save();
+        //==========================================================================
+
+        
+        //se houver alguma falha lance a mensagem
+        if($user->fail()){
+            header("HTTP/1.1 500 Internal server Error");
+            echo json_encode(array("response" => $user->fail()->getMessage()));
+            exit;
+        }
+
+        header("HTTP/1.1 201 Created");
+        echo json_encode(array("response" => "Usuário atualizado com sucesso"));
+    break;
     default:
         //definindo um cabeçalho
         header("HTTP/1.1 401 Unauthorized");
